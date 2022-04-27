ac65331f4504d5aa0a4e142b8cf573e5d019218e
catatsuy <catatsuy@catatsuy.org>
2022-04-27 13:01:25 +0900

    miss

--- golang/main.go
+++ golang/main.go
@@ -473,12 +473,12 @@ func getRecentPlaylistSummaries(ctx context.Context, db connOrTx, userAccount st
 func getPopularPlaylistSummaries(ctx context.Context, db connOrTx, userAccount string) ([]Playlist, error) {
 	var popular []struct {
 		PlaylistID    int `db:"playlist_id"`
-		FavoriteCount int `db:"favorite_count"`
+		FavoriteCount int `db:"count"`
 	}
 	if err := db.SelectContext(
 		ctx,
 		&popular,
-		`SELECT playlist_id, favorite_count FROM playlist_favorite_count ORDER BY favorite_count DESC`,
+		`SELECT playlist_id, count FROM playlist_favorite_count ORDER BY count DESC`,
 	); err != nil {
 		return nil, fmt.Errorf(
 			"error Select playlist_favorite: %w",
